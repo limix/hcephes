99b5f6996646c1ee23af71e5d418e7905641a99f
Danilo Horta <danilo.horta@gmail.com>
2017-09-15 15:52:07 +0100

    clang-format

--- ncephes/cprob/expx2.c
+++ ncephes/cprob/expx2.c
@@ -1,45 +1,8 @@
-/*							expx2.c
- *
- *	Exponential of squared argument
- *
- *
- *
- * SYNOPSIS:
- *
- * double x, y, expx2();
- * int sign;
- *
- * y = expx2( x, sign );
- *
- *
- *
- * DESCRIPTION:
- *
- * Computes y = exp(x*x) while suppressing error amplification
- * that would ordinarily arise from the inexactness of the
- * exponential argument x*x.
- *
- * If sign < 0, the result is inverted; i.e., y = exp(-x*x) .
- *
- *
- * ACCURACY:
- *
- *                      Relative error:
- * arithmetic    domain     # trials      peak         rms
- *   IEEE      -26.6, 26.6    10^7       3.9e-16     8.9e-17
- *
- */
-
-/*
-Cephes Math Library Release 2.9:  June, 2000
-Copyright 2000 by Stephen L. Moshier
-*/
-
 #include "mconf.h"
 
-extern double fabs (double);
-extern double floor (double);
-extern double exp (double);
+extern double fabs(double);
+extern double floor(double);
+extern double exp(double);
 
 #ifdef DEC
 #define M 32.0
@@ -52,12 +15,10 @@ extern double exp (double);
 extern double MAXLOG;
 extern double NCEPHES_INF;
 
-double
-expx2 (double x, int sign)
-{
+double ncephes_expx2(double x, int sign) {
   double u, u1, m, f;
 
-  x = fabs (x);
+  x = fabs(x);
   if (sign < 0)
     x = -x;
 
@@ -69,18 +30,17 @@ expx2 (double x, int sign)
 
   /* x^2 = m^2 + 2mf + f^2 */
   u = m * m;
-  u1 = 2 * m * f  +  f * f;
+  u1 = 2 * m * f + f * f;
 
-  if (sign < 0)
-    {
-      u = -u;
-      u1 = -u1;
-    }
+  if (sign < 0) {
+    u = -u;
+    u1 = -u1;
+  }
 
-  if ((u+u1) > MAXLOG)
+  if ((u + u1) > MAXLOG)
     return (NCEPHES_INF);
 
   /* u is exact, u1 is small.  */
   u = exp(u) * exp(u1);
-  return(u);
+  return (u);
 }
